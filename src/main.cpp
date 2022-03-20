81d51b70d19802e4b42cf48a4c8eee6e67c187ce
George Kirby <georgekirby2003@gmail.com>
2022-03-20 20:29:35 +0000

    function update

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 int ledPin = 13;
 int delayPeriod = 100;
+
+
 void setup() {
   Serial.begin(9600);
   Serial.println(123);
   pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
+void flash(){
   digitalWrite(ledPin, HIGH);
   delay(delayPeriod);
   digitalWrite(ledPin, LOW);
   delay(delayPeriod);
-  delayPeriod = delayPeriod + 100;
 }
+
+void loop() {
+  for (int i = 0; i <20; i++){
+  flash();
+  }
+  delay(3000);
+}
+
